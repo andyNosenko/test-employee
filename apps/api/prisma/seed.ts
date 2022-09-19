f3c6f46ad4266a2991b941e51dacad08cfce99e9
Andrey Nosenko <a1996y@yandex.by>
2022-09-19 19:43:36 +0300

    add prisma seeds

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,36 @@
+import {roles} from './seed-data/roles';
+import {employees} from './seed-data/employees';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    for (let employee of employees) {
+        await prisma.employee.upsert({
+            where: {
+                id: employee.id,
+            },
+            create: employee,
+            update: employee,
+        });
+    }
+    for (let role of roles) {
+        await prisma.employeeRole.upsert({
+            where: {
+                id: role.id,
+            },
+            create: role,
+            update: role,
+        });
+    }
+
+}
+
+main()
+    .catch(e => {
+        console.log(e);
+        process.exit(1);
+    })
+    .finally(() => {
+        prisma.$disconnect();
+    });
